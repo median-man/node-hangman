5c9bf585094d1558ffb82f6044ad44a4ec0a19ed
John Desrosiers <johndrunner@yahoo.com>
2017-12-28 11:42:03 -0800

    modified Letter constructor

--- src/letter.js
+++ src/letter.js
@@ -1,13 +1,15 @@
 function Letter(value, placeholder) {
-  if (typeof value !== 'string' || value.length !== 1 || !/[a-z]/i.test(value)) {
+  // regex to test for a valide value (placeholders may be any string not matched by value test)
+  const reValueTest = /[a-z ]/i;
+  if (typeof value !== 'string' || value.length !== 1 || !reValueTest.test(value)) {
     throw new Error('value must be a letter');
   }
-  if (typeof placeholder !== 'string' || placeholder.length === 0) {
+  if (typeof placeholder !== 'string' || placeholder.length === 0 || reValueTest.test(placeholder)) {
     throw new Error('placeholder must be a non-empty string');
   }
   this.value = value;
   this.placeholder = placeholder;
-  this.isHidden = true;
+  this.isHidden = value !== ' ';
 }
 Letter.prototype.toString = function letterToString() {
   return this.isHidden ? this.placeholder : this.value;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 
 const Letter = require('../src/letter.js');
+const Word = require('../src/word.js');
 
 // unit tests for Letter class
 describe('Letter', () => {
@@ -14,17 +15,20 @@ describe('Letter', () => {
       expect(new Letter('a', '_')).to.haveOwnProperty('placeholder');
       expect(new Letter('a', '_').placeholder).to.equal('_');
     });
-    it('throws if the letter is not a valid string for a single letter', () => {
+    it('throws if the letter is not a valid string for a single letter or space', () => {
       const throwTests = [null, { a: 'a' }, 5, '', 'ab', '5', '_b', '_'];
       expect(() => new Letter(), 'no arguements').to.throw();
       expect(() => new Letter('Z', '_'), 'Z').to.not.throw();
+      expect(() => new Letter(' ', '_'), '" "').to.not.throw();
       throwTests.forEach((value) => {
         expect(() => new Letter(value, '_'), `value = ${value}`).to.throw();
       });
     });
-    it('throws if the placeholder is not a string with a length of at least 1', () => {
+    it('throws if placeholder is not a valid placeholder string', () => {
       const throwTests = [null, '', 5];
-      const passTests = ['_', 'abc', ' '];
+      const passTests = ['_', '--', '0'];
+      // must not be a space
+      expect(() => new Letter('a', ' '), 'single space').to.throw();
       throwTests.forEach((placeholder) => {
         expect(() => new Letter('a', placeholder), `placeholder = ${placeholder}`).to.throw();
       });
@@ -32,26 +36,39 @@ describe('Letter', () => {
         expect(() => new Letter('a', placeholder), `placeholder = ${placeholder}`).to.not.throw();
       });
     });
-    it('sets the isHidden property to true', () => {
-      expect(new Letter('a', ' ').isHidden).to.be.true;
+    it('sets the isHidden property to true if letter is not a space', () => {
+      expect(new Letter('a', '_').isHidden).to.be.true;
+    });
+    it('sets the isHidden property to false if letter is a space', () => {
+      expect(new Letter(' ', '_').isHidden).to.be.false;
     });
   });
   describe('prototype', () => {
     describe('toString', () => {
       it('returns the value property if isHidden is false', () => {
-        const letter = new Letter('a', ' ');
+        const letter = new Letter('a', '_');
         letter.isHidden = false;
         expect(letter.isHidden, 'precondition not met').to.be.false;
         expect(letter.value, 'precondition not met').to.equal('a');
         expect(letter.toString()).to.equal('a');
       });
       it('returns the placeholder property if isHidden is true', () => {
-        const letter = new Letter('a', ' ');
+        const letter = new Letter('a', '-');
         letter.isHidden = true;
         expect(letter.isHidden, 'precondition not met').to.be.true;
-        expect(letter.placeholder, 'precondition not met').to.equal(' ');
-        expect(letter.toString()).to.equal(' ');
+        expect(letter.placeholder, 'precondition not met').to.equal('-');
+        expect(letter.toString()).to.equal('-');
       });
     });
+  });
+});
+
+// unit tests for Word class
+describe.skip('Word', () => {
+  describe('constructor', () => {
+    it('accepts a word parameter');
+    it('throws if the word is not a string');
+    it('throws if the word is an empty string');
+    it('adds a letter object to letters for each letter in the word');
   });
 });
